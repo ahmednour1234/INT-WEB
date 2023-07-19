383aa0d17a430e068b3a4fb0fcd487639a1024a3
ahmednour1234 <106186787+ahmednour1234@users.noreply.github.com>
2023-07-20 01:57:35 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -196,7 +196,12 @@ a {
     overflow: hidden;
     justify-content: center;
     display: flex;
+  animation: fadeIn 5s;
   }
+  @keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
   i {
     visibility: visible;
   }
